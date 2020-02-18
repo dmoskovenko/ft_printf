6d65f18a09e30728f007f8a02e1005714bd739f1
Ripcord Eleanora <releanor@at-j2.21-school.ru>
2020-02-18 20:22:41 +0300

    Added struct, norminetted

--- ft_printf.h
+++ ft_printf.h
@@ -6,10 +6,32 @@
 /*   By: releanor <releanor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 20:15:37 by releanor          #+#    #+#             */
-/*   Updated: 2020/02/18 20:15:47 by releanor         ###   ########.fr       */
+/*   Updated: 2020/02/18 20:22:11 by releanor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <stdio.h>
-#include "libft/includes/libft.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdio.h>
+# include "libft/includes/libft.h"
+
+typedef struct	s_struct
+{
+	char		*fmt;
+	int			nprinted;
+	int			i;
+	int			len;
+	int			minus;
+	int			plus;
+	int			space;
+	int			zero;
+	int			hash;
+	int			width;
+	int			precisiontf;
+	int			precision;
+	int			length;
+}				t_struct;
+
+#endif
