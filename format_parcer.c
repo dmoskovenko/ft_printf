c0bbee9b3ddcd0e0c35c33c0e000935760b3c0ba
Ripcord Eleanora <releanor@at-j2.21-school.ru>
2020-02-19 00:13:08 +0300

    Added format_parcer.c & format_parse()

--- format_parcer.c
+++ format_parcer.c
@@ -6,8 +6,36 @@
 /*   By: releanor <releanor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 23:35:59 by releanor          #+#    #+#             */
-/*   Updated: 2020/02/18 23:40:50 by releanor         ###   ########.fr       */
+/*   Updated: 2020/02/19 00:12:25 by releanor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-cvbxc bzgdflbdffdzdfbafbdafbvdafb
+#include "ft_printf.h"
+
+int		format_parse(const char *format, t_struct *list, va_list ap, int pos)
+{
+	while (format[pos] != '\0')
+	{
+		if (format[pos] != '%' && format[pos])
+			list->nprinted += write(1, &format[pos], 1);
+		else if (format[pos] == '%')
+		{
+			if (!ft_strchr(ALLSYMBOLS, format[pos + 1]))       // need to define ALLSYMBOLS
+				break;
+			while (ft_strchr(ALLSYMBOLS, format[pos + 1]))
+			{
+				pos += 1;
+				if (ft_strchr("cspdiouxXfyb%", format[pos]))   //we don't need all these identifiers
+				{
+					pos = parsel2(list, pos, format, ap) + 2;  //maybe change the name of this func
+					break;
+				}
+				else
+					pos = parsel2(list, pos, format, ap);
+			}
+			continue;
+		}
+		pos++;
+	}
+	return (list->nprinted);
+}
