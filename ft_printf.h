bd93b819d078051af119f60e3642eec968df84d2
Concetta Clayton <coclayto@at-j4.21-school.ru>
2020-02-19 00:42:38 +0300

    restructured head func ft_printf & added VALIDSYM macros

--- ft_printf.h
+++ ft_printf.h
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: releanor <releanor@student.42.fr>          +#+  +:+       +#+        */
+/*   By: coclayto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 20:15:37 by releanor          #+#    #+#             */
-/*   Updated: 2020/02/18 20:22:11 by releanor         ###   ########.fr       */
+/*   Updated: 2020/02/19 00:39:48 by coclayto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-
+# define VALIDSYM		"cspdiouxXfhl0123456789 %#-+."
 # include <stdarg.h>
 # include <stdio.h>
 # include "libft/includes/libft.h"
---
+++ main.c
@@ -0,0 +1,6 @@
+int		main(void)
+{
+	ft_printf("digit: %d %s\n", 30, "tis");
+	printf("digit: %d %s\n", 30, "tis");
+	return (0);
+}
