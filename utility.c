5f549f7dd97e8ee52c82725eedf32e16f893011b
Ripcord Eleanora <releanor@at-q3.21-school.ru>
2020-03-02 16:28:32 +0300

    Polished oct

--- utility.c
+++ utility.c
@@ -6,7 +6,7 @@
 /*   By: releanor <releanor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 12:55:23 by releanor          #+#    #+#             */
-/*   Updated: 2020/03/02 15:38:20 by releanor         ###   ########.fr       */
+/*   Updated: 2020/03/02 16:27:31 by releanor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,14 +68,9 @@ char	*itoa_base_unsigned(unsigned int num, int base/*, int signe*/)  //кита�
 {
 	char 	*out;
 	int 	i;
-	// int		is_neg;
 
 	i = unsigned_num_len(num, base);
-	// i = (signe == 0) ? unsigned_num_len(num, base) : signed_num_len(num, base);
-	// num = (signe == 0) ? (unsigned int)num : num; 
-	// is_neg = (num < 0 && base == 10) ? 1 : 0;
-	// num = (num < 0 && base == 10) ? -num : num;
-	out = (char *)malloc(sizeof(char) * (/*is_neg + */i + 1));
+	out = (char *)malloc(sizeof(char) * (i + 1));
 	out[i--] = '\0';
 	while (i >= 0)
 	{
@@ -84,7 +79,6 @@ char	*itoa_base_unsigned(unsigned int num, int base/*, int signe*/)  //кита�
 		num /= base;
 		i--;
 	}
-    // out[0] = (is_neg == 1) ? '-' : out[0];
 	return (out);
 }
 
