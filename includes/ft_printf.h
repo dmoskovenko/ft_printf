06bf6ecc60853d016c6b27a3100b6f348ad67fe8
Dmitry <moskovenko.dmitry@yandex.ru>
2020-03-06 18:14:13 +0300

    delete unnecessary files

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -17,7 +17,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdint.h>
-# include "libft/includes/libft.h"
+# include "../libft/includes/libft.h"
 
 # define VALIDSYM		"cCsSpPdDiIoOuUxXfFhlL0123456789 %#-+."
 # define TYPESYM		"cCsSpPdDiIoOuUxXfF"
